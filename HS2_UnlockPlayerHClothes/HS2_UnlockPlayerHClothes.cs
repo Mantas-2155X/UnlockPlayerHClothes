42d1b4e225c356e60f8c770efbe03bf54e32807b
Mantas-2155X <syenergyy@gmail.com>
2020-06-24 22:46:17 +0300

    * Fix 2nd male clothes state not being set if 1st male is null * No need to cache HScene

--- HS2_UnlockPlayerHClothes/HS2_UnlockPlayerHClothes.cs
+++ HS2_UnlockPlayerHClothes/HS2_UnlockPlayerHClothes.cs
@@ -15,7 +15,6 @@ namespace HS2_UnlockPlayerHClothes {
         
         public new static ManualLogSource Logger;
 
-        private static HScene hScene;
         private static readonly List<int> clothesKindList = new List<int>{0, 2, 4, 1, 3, 5, 6};
 
         private void Awake()
@@ -30,28 +29,26 @@ namespace HS2_UnlockPlayerHClothes {
         [HarmonyPostfix, HarmonyPatch(typeof(HScene), "SetStartVoice")]
         public static void HScene_SetStartVoice_ApplyClothesConfig(HScene __instance)
         {
-            hScene = __instance;
-            
-            var males = hScene.GetMales();
-            if (males[0] == null)
-                return;
-            
             var hData = Manager.Config.HData;
-            
-            foreach (var kind in clothesKindList.Where(kind => males[0].IsClothesStateKind(kind)))
-                males[0].SetClothesState(kind, (byte)(hData.Cloth ? 0 : 2));
-            
-            males[0].SetAccessoryStateAll(hData.Accessory);
-            males[0].SetClothesState(7, (byte)(!hData.Shoes ? 2 : 0));
+            var males = __instance.GetMales();
 
-            if (males[1] == null)
-                return;
+            if (males[0] != null)
+            {
+                foreach (var kind in clothesKindList.Where(kind => males[0].IsClothesStateKind(kind)))
+                    males[0].SetClothesState(kind, (byte)(hData.Cloth ? 0 : 2));
             
-            foreach (var kind in clothesKindList.Where(kind => males[1].IsClothesStateKind(kind)))
-                males[1].SetClothesState(kind, (byte)(hData.SecondCloth ? 0 : 2));
+                males[0].SetAccessoryStateAll(hData.Accessory);
+                males[0].SetClothesState(7, (byte)(!hData.Shoes ? 2 : 0));
+            }
             
-            males[1].SetAccessoryStateAll(hData.SecondAccessory);
-            males[1].SetClothesState(7, (byte)(!hData.SecondShoes ? 2 : 0));
+            if (males[1] != null)
+            {
+                foreach (var kind in clothesKindList.Where(kind => males[1].IsClothesStateKind(kind)))
+                    males[1].SetClothesState(kind, (byte)(hData.SecondCloth ? 0 : 2));
+            
+                males[1].SetAccessoryStateAll(hData.SecondAccessory);
+                males[1].SetClothesState(7, (byte)(!hData.SecondShoes ? 2 : 0));
+            }
         }
     }
 }
